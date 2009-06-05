7a79b1fba76644b84464a42e9653b879b707de33
Jern_cp <SND\Jern_cp>
2009-06-05 10:26:04 +0000

    Can Zip Projects and Items. No GUI yet

---
+++ SolutionZipperConsoleTest/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SolutionZipper;
+using System.IO;
+
+namespace SolutionZipperConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Test1();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception: " + ex.ToString());
+            }
+            finally
+            {
+                Console.WriteLine("Press Any Key");
+                Console.ReadLine();
+            }
+        }
+
+        private static void Test1()
+        {
+            using (var controller = new SolZipController(@"C:\Funky.zip"))
+            {
+                controller.ZipSolution(@"C:\Projects\KbhKommune.Ask.backup\KbhKommune.Ask.sln");
+            }
+            
+        }
+
+        private static void Test2()
+        {
+            Console.WriteLine(File.Exists(Path.Combine(@"C:\Projects\KbhKommune.Ask.backup\CrmDataGateway", @"..\microsoft.crm.sdktypeproxy.dll")));
+        }
+    }
+}
