35b41af99553a8b85f5f9bcbea7bcecc51cea8d5
Jern_cp <SND\Jern_cp>
2009-06-05 14:10:28 +0000

    Work in progress on Command Line tool

---
+++ SolutionZipper/SolZipHelper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace SolutionZipper
+{
+    public static class SolZipHelper
+    {
+        public static string SuggestFileName(string fileToZip)
+        {
+            return SuggestFileName(Path.Combine(Path.GetPathRoot(fileToZip), Path.GetFileNameWithoutExtension(fileToZip)), 0);
+        }
+
+        private static string SuggestFileName(string fileNoExtension, int seed)
+        {
+            string suggestion = string.Empty;
+            if(seed == 0)
+            {
+                suggestion = fileNoExtension + ".zip";  
+            }
+            else
+            {
+                suggestion = string.Format("{0}.{1}.zip", fileNoExtension, seed.ToString());
+            }
+            if (File.Exists(suggestion))
+                return SuggestFileName(fileNoExtension, seed++);
+
+            return suggestion;
+        }
+
+        public static Dictionary<string, string> ArgsToDictionary(string[] args)
+        {
+            return args.ToDictionary(arg => arg.Split(new char[] { ':' }, 2)[0].ToLower(), arg => (arg != null && arg.Contains(':')) ? arg.Split(new char[] { ':' }, 2)[1] : string.Empty); 
+            
+        }
+    }
+}
---
+++ SunZip/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SolutionZipper;
+
+namespace SunZip
+{
+    /// <summary>
+    /// SunZip is the conmmandline entrance to Zipping VS Solutions, CSharpProjects, SetupProjects and Items
+    /// Lovingly called SunZip because sol in danish means sun.
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Allowed Arguments
+        ///     /? To Display help - same as no arguments
+        ///     /ZipFile:"name" The filename of the ZipFile. 
+        ///     /Solution:"name" The filename of the solution to Zip. 
+        ///     /Project:"name" The filename of the C# Project to Zip. 
+        ///     /SetupProject:"name" The filename of the setup project to Zip. 
+        ///     /File:"name" The file to Zip.
+        ///     Only one of /Solution, /Project and /SetupProject can be provided. If no name is provided, first item in current dir is chosen
+        ///     If full filename is not provided the file will be assumed to be in current dir.
+        ///     If no filename is provided for Zipfile a name is generated.
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            try
+            {
+                Dictionary<string, string> argDic = SolZipHelper.ArgsToDictionary(args);
+                if (argDic == null || argDic.Count() == 0 || argDic.ContainsKey("/?"))
+                {
+                    DisplayHelp();
+                    return;
+                }
+
+                if (!argDic.ContainsKey("/solution") && !argDic.ContainsKey("/project") && !argDic.ContainsKey("/setupproject") && !argDic.ContainsKey("/file"))
+                {
+                    DisplayHelp();
+                    return;
+                }
+
+                m_ZipFileName = GetZipFileName(argDic, @"C:\Eksempel.txt");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception: " + ex.ToString());
+            }
+#if DEBUG
+            finally
+            {
+                Console.WriteLine("Press Any Key");
+                Console.ReadLine();
+            }
+#endif
+        }
+
+        private static string m_ZipFileName;
+        private static string m_FileToZip;
+
+        private static void DisplayHelp()
+        {
+            Console.WriteLine("SunZip usage:");
+            Console.WriteLine("    /? To Display help - same as no arguments");
+            Console.WriteLine("    /ZipFile:\"name\" The filename of the ZipFile.");
+            Console.WriteLine("    /Solution:\"name\" The filename of the solution to Zip.");
+            Console.WriteLine("    /Project:\"name\" The filename of the C# Project to Zip.");
+            Console.WriteLine("    /SetupProject:\"name\" The filename of the setup project to Zip.");
+            Console.WriteLine("    /File:\"name\" The file to Zip.");
+            Console.WriteLine();
+            Console.WriteLine("    Only one of /Solution, /Project and /SetupProject can be provided.");
+            Console.WriteLine("    If no name is provided, first item in current directory is chosen.");
+            Console.WriteLine();
+            Console.WriteLine("    If no filename is provided for Zipfile a name is generated.");
+        }
+
+        private static string GetZipFileName(Dictionary<string, string> args, string fileToZip)
+        {
+            if (!args.ContainsKey("/zipfile") || string.IsNullOrEmpty(args["/zipfile"]))
+                return SolZipHelper.SuggestFileName(fileToZip);
+
+            return args["/zipfile"];
+        }
+    }
+}
