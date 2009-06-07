2c395c39edc9c2824f199c990d0327f6a0875a43
Jern_cp <jesper@niedermann.dk>
2009-06-07 20:50:42 +0000

    Guidance package appears in solution explorer does not work yet

--- SolZipGuidanceGAT/Properties/AssemblyInfo.cs
+++ SolZipGuidanceGAT/Properties/AssemblyInfo.cs
@@ -1,5 +1,5 @@
-﻿using System.Reflection;
+using System.Reflection;
 
-[assembly: AssemblyTitle("SunZip")]
+[assembly: AssemblyTitle("SolZipGuidanceGAT")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
