0c7825684a5832d0415d5e1ba28138b6b3fffb4a
Jern_cp <SND\Jern_cp>
2009-06-07 08:56:33 +0000

    Commandline experience SunZip finished

--- SolutionZipper/SolZipHelper.cs
+++ SolutionZipper/SolZipHelper.cs
@@ -8,26 +8,42 @@ namespace SolutionZipper
 {
     public static class SolZipHelper
     {
-        public static string SuggestFileName(string fileToZip)
+        private static string SuggestFileName(string fileToZip)
         {
-            return SuggestFileName(Path.Combine(Path.GetPathRoot(fileToZip), Path.GetFileNameWithoutExtension(fileToZip)), 0);
+            return SuggestFileName(Path.Combine(Path.GetDirectoryName(fileToZip), Path.GetFileNameWithoutExtension(fileToZip)),
+                GetZipFileSuffix(fileToZip), 0);
         }
 
-        private static string SuggestFileName(string fileNoExtension, int seed)
+        private static string SuggestFileName(string fileNoExtension, string zipFileSuffix, int seed)
         {
             string suggestion = string.Empty;
             if(seed == 0)
             {
-                suggestion = fileNoExtension + ".zip";  
+                suggestion = fileNoExtension + zipFileSuffix + ".zip";  
             }
             else
             {
-                suggestion = string.Format("{0}.{1}.zip", fileNoExtension, seed.ToString());
+                suggestion = string.Format("{0}{1}{2}.zip", fileNoExtension, zipFileSuffix, seed.ToString());
             }
             if (File.Exists(suggestion))
-                return SuggestFileName(fileNoExtension, seed++);
+                return SuggestFileName(fileNoExtension, zipFileSuffix, seed+1);
 
             return suggestion;
+        }
+
+        private static string GetZipFileSuffix(string fileToZip)
+        {
+            switch (Path.GetExtension(fileToZip))
+            {
+                case SolZipConstants.SolutionExtension:
+                    return SolZipConstants.SolutionSuffix; 
+                case SolZipConstants.CsharpProjectExtension:
+                    return SolZipConstants.ProjectSuffix; 
+                case SolZipConstants.SetupProjectExtension:
+                    return SolZipConstants.SetupSuffix;
+                default:
+                    return SolZipConstants.ItemSuffix;
+            }
         }
 
         public static Dictionary<string, string> ArgsToDictionary(string[] args)
@@ -35,5 +51,72 @@ namespace SolutionZipper
             return args.ToDictionary(arg => arg.Split(new char[] { ':' }, 2)[0].ToLower(), arg => (arg != null && arg.Contains(':')) ? arg.Split(new char[] { ':' }, 2)[1] : string.Empty); 
             
         }
+
+        public static string GetZipFileName(string fileToZip)
+        {
+            return SolZipHelper.SuggestFileName(fileToZip);
+        }
+
+        public static void ZipItem(string zipFileName, string fileToZip)
+        {
+            using (var controller = new SolZipController(zipFileName))
+            {
+                controller.ZipFile(fileToZip);
+            }
+        }
+
+        public static void ZipSetupProject(string zipFileName, string fileToZip)
+        {
+            using (var controller = new SolZipController(zipFileName))
+            {
+                controller.ZipSetupProject(fileToZip);
+            }
+        }
+
+        public static void ZipProject(string zipFileName, string fileToZip)
+        {
+            using (var controller = new SolZipController(zipFileName))
+            {
+                controller.ZipProject(fileToZip);
+            }
+        }
+
+        public static void ZipSolution(string zipFileName, string fileToZip)
+        {
+            using (var controller = new SolZipController(zipFileName))
+            {
+                controller.ZipSolution(fileToZip);
+            }
+        }
+
+        public static void Zip(string zipFileName, string fileToZip)
+        {
+            using (var controller = new SolZipController(zipFileName))
+            {
+                switch (GetZipFileSuffix(fileToZip))
+                {
+                    case SolZipConstants.SolutionSuffix:
+                        controller.ZipSolution(fileToZip);
+                        break;
+                    case SolZipConstants.ProjectSuffix:
+                        controller.ZipProject(fileToZip);
+                        break;
+                    case SolZipConstants.SetupSuffix:
+                        controller.ZipSetupProject(fileToZip);
+                        break;
+                    case SolZipConstants.ItemSuffix:
+                        controller.ZipFile(fileToZip);
+                        break;
+                    default:
+                        throw new ArgumentException("Suffix of file to zip not reqcognized", "fileToZip");
+                }
+            }
+        }
+
+        public static void Zip(string fileToZip)
+        {
+            Zip(GetZipFileName(fileToZip), fileToZip);
+        }
+
     }
 }
--- SunZip/Program.cs
+++ SunZip/Program.cs
@@ -30,19 +30,13 @@ namespace SunZip
             try
             {
                 Dictionary<string, string> argDic = SolZipHelper.ArgsToDictionary(args);
-                if (argDic == null || argDic.Count() == 0 || argDic.ContainsKey("/?"))
+                if (MustDisplayHelp(argDic))
                 {
                     DisplayHelp();
                     return;
                 }
 
-                if (!argDic.ContainsKey("/solution") && !argDic.ContainsKey("/project") && !argDic.ContainsKey("/setupproject") && !argDic.ContainsKey("/file"))
-                {
-                    DisplayHelp();
-                    return;
-                }
-
-                m_ZipFileName = GetZipFileName(argDic, @"C:\Eksempel.txt");
+                Zip(argDic);
             }
             catch (Exception ex)
             {
@@ -57,31 +51,142 @@ namespace SunZip
 #endif
         }
 
-        private static string m_ZipFileName;
-        private static string m_FileToZip;
-
         private static void DisplayHelp()
         {
             Console.WriteLine("SunZip usage:");
-            Console.WriteLine("    /? To Display help - same as no arguments");
-            Console.WriteLine("    /ZipFile:\"name\" The filename of the ZipFile.");
-            Console.WriteLine("    /Solution:\"name\" The filename of the solution to Zip.");
-            Console.WriteLine("    /Project:\"name\" The filename of the C# Project to Zip.");
-            Console.WriteLine("    /SetupProject:\"name\" The filename of the setup project to Zip.");
-            Console.WriteLine("    /File:\"name\" The file to Zip.");
+            Console.WriteLine("    {0} To Display help - same as no arguments", SolZipConstants.HelpArgument);
+            Console.WriteLine("    {0}:\"name\" The filename of the ZipFile.", SolZipConstants.ZipFileArgument);
+            Console.WriteLine("    {0}:\"name\" The filename of the solution to Zip.", SolZipConstants.SolutionArgument);
+            Console.WriteLine("    {0}:\"name\" The filename of the C# Project to Zip.", SolZipConstants.ProjectArgument);
+            Console.WriteLine("    {0}:\"name\" The filename of the setup project to Zip.", SolZipConstants.SetupProjectArgument);
+            Console.WriteLine("    {0}:\"name\" The file to Zip.", SolZipConstants.FileArgument);
             Console.WriteLine();
-            Console.WriteLine("    Only one of /Solution, /Project and /SetupProject can be provided.");
+            Console.WriteLine("    Only one of {0}, {1}, {2} and {3} can be provided.", 
+                SolZipConstants.SolutionArgument, SolZipConstants.ProjectArgument, SolZipConstants.SetupProjectArgument, 
+                SolZipConstants.FileArgument);
             Console.WriteLine("    If no name is provided, first item in current directory is chosen.");
             Console.WriteLine();
             Console.WriteLine("    If no filename is provided for Zipfile a name is generated.");
+            Console.WriteLine();
+            Console.WriteLine("    Examples:");
+            Console.WriteLine("      SunZip {0}:\"C:\\Eksempel.zip\" {1}:\"C:\\Eksempel\\Eksempel.sln\"", SolZipConstants.ZipFileArgument, SolZipConstants.SolutionArgument);
+            Console.WriteLine("      SunZip {0}:\"C:\\Eksempel\\Eksempel\\Eksempel.csproj\"", SolZipConstants.ProjectArgument);
         }
 
         private static string GetZipFileName(Dictionary<string, string> args, string fileToZip)
         {
-            if (!args.ContainsKey("/zipfile") || string.IsNullOrEmpty(args["/zipfile"]))
-                return SolZipHelper.SuggestFileName(fileToZip);
+            if (!args.ContainsKey(SolZipConstants.ZipFileArgument) || string.IsNullOrEmpty(args[SolZipConstants.ZipFileArgument]))
+                return SolZipHelper.GetZipFileName(fileToZip);
 
-            return args["/zipfile"];
+            return args[SolZipConstants.ZipFileArgument];
+        }
+
+        /// <summary>
+        /// Number of correct arguments must equal one, since only one of Solution, Project, SetupProject, File is allowed
+        /// This method counts the number of correct arguments. /zipfile is not counted, since it is optional.
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        private static int NumberOfCorrectArguments(Dictionary<string, string> args)
+        {
+            int count = 0;
+            var correctArguments = new string[] { SolZipConstants.SolutionArgument, SolZipConstants.ProjectArgument, 
+                SolZipConstants.SetupProjectArgument, SolZipConstants.FileArgument };
+            foreach (var arg in args.Keys)
+            {
+                if (arg == SolZipConstants.ZipFileArgument)
+                {
+                    //OK, not counted since it is optional
+                }
+                else if (correctArguments.Contains(arg))
+                {
+                    count++;
+                }
+                else
+                {
+                    return -1;
+                }
+            }
+            return count;
+        }
+
+        /// <summary>
+        /// Number of correct arguments must equal one, since only one of Solution, Project, SetupProject, File is allowed
+        /// This method counts the number of correct arguments. /zipfile is not counted, since it is optional.
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        private static bool IsValidArguments(Dictionary<string, string> args)
+        {
+            if (args == null || args.Count() == 0)
+                return false;
+
+            return (NumberOfCorrectArguments(args) == 1);
+        }
+
+        private static bool MustDisplayHelp(Dictionary<string, string> args)
+        {
+            return !IsValidArguments(args) || args.ContainsKey(SolZipConstants.HelpArgument);
+        }
+
+        private static string GetSolutionArgument(Dictionary<string, string> args)
+        {
+            return GetArgument(args, SolZipConstants.SolutionArgument); ;
+        }
+
+        private static string GetProjectArgument(Dictionary<string, string> args)
+        {
+            return GetArgument(args, SolZipConstants.ProjectArgument); ;
+        }
+
+        private static string GetSetupProjectArgument(Dictionary<string, string> args)
+        {
+            return GetArgument(args, SolZipConstants.SetupProjectArgument); ;
+        }
+
+        private static string GetFileArgument(Dictionary<string, string> args)
+        {
+            return GetArgument(args, SolZipConstants.FileArgument); ;
+        }
+
+        private static string GetArgument(Dictionary<string, string> args, string key)
+        {
+            string argValue = string.Empty;
+            if (args.ContainsKey(key))
+            {
+                argValue = args[key];
+            }
+            return argValue;
+        }
+
+        private static void Zip(Dictionary<string, string> args)
+        {
+            string solutionFile = GetSolutionArgument(args);
+            string projectFile = GetProjectArgument(args);
+            string setupProjectFile = GetSetupProjectArgument(args);
+            string itemFile = GetFileArgument(args); 
+
+            if (!string.IsNullOrEmpty(solutionFile))
+            {
+                SolZipHelper.ZipSolution(GetZipFileName(args, solutionFile), solutionFile); 
+            }
+            else if (!string.IsNullOrEmpty(projectFile))
+            {
+                SolZipHelper.ZipProject(GetZipFileName(args, projectFile), projectFile);
+            }
+            else if (!string.IsNullOrEmpty(setupProjectFile))
+            {
+                SolZipHelper.ZipSetupProject(GetZipFileName(args, setupProjectFile), setupProjectFile);
+            }
+            else if (!string.IsNullOrEmpty(itemFile))
+            {
+                SolZipHelper.ZipItem(GetZipFileName(args, itemFile), itemFile);
+            }
         }
     }
+
+    //Sample Test strings
+    // /zipfile:"C:\Eks\Eks.zip" /solution:"C:\Projects\KbhKommune.Ask.backup\KbhKommune.Ask.sln"
+    // /project:"C:\Projects\KbhKommune.Ask.backup\CrmDataGateway\CrmDataGateway.csproj"
+    // /file:"C:\Projects\KbhKommune.Ask.backup\CrmDataGateway\CrmDataGateway.csproj"
 }
