a20e06dbb4a9d48a263c33f927a0db69e19cf32f
Jern_cp <jesper@niedermann.dk>
2009-06-08 22:37:02 +0000

    Implemented Copy to Clipboard in SolZipGuidance

---
+++ SolZipGuidance/Actions/CopyToClipboardAction.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Practices.RecipeFramework;
+using System.IO;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace SolZipGuidance.Actions
+{
+    public class CopyToClipboardAction : ConfigurableAction
+    {
+        [Input]
+        public string TextToCopy { get; set; }
+
+        /// <summary>
+        /// Copys the input text to the clipboard
+        /// </summary>
+        public override void Execute()
+        {
+            Clipboard.SetText(TextToCopy);
+        }
+
+        public override void Undo()
+        {
+            //Not relevant
+        }
+    }
+}
