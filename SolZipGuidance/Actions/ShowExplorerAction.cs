d5457b51323da87917e568fa3ad155af91daa0c8
Jern_cp <SND\Jern_cp>
2009-06-08 14:52:31 +0000

    Guidance Automation 99% finished, So is commandline util. Now on to ManagedMenuExtensions

---
+++ SolZipGuidance/Actions/ShowExplorerAction.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Practices.RecipeFramework;
+using System.IO;
+using System.Diagnostics;
+
+namespace SolZipGuidance.Actions
+{
+    public class ShowExplorerAction : ConfigurableAction
+    {
+        [Input]
+        public string Folder { get; set; }
+
+        /// <summary>
+        /// Shows the explorer in the given folder.
+        /// It is very forgiven. If we provide a file it opens the folder in which the file is placed.
+        /// </summary>
+        public override void Execute()
+        {
+            if (File.Exists(Folder)) //It is really a file
+            {
+                Folder = Path.GetDirectoryName(Folder);
+            }
+            if (!Directory.Exists(Folder))
+                throw new ArgumentException("You must provide a real folder name for ShowExplorerAction", "Folder");
+
+            Process.Start(Folder);
+        }
+
+        public override void Undo()
+        {
+            //Not relevant
+        }
+    }
+}
---
+++ SolZipGuidance/ValueProviders/ZipFileNameProvider.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SolutionZipper;
+using Microsoft.Practices.RecipeFramework;
+using Microsoft.Practices.Common;
+using System.Collections.Specialized;
+using System.ComponentModel.Design;
+
+namespace SolZipGuidance.ValueProviders
+{
+    /// <summary>
+    /// Given a filename it suggest a filename for the ZipFile
+    /// </summary>
+    public class ZipFileNameProvider : ValueProvider, IAttributesConfigurable
+    {
+        private string m_RecipeArgumentName;
+
+        public override bool OnBeginRecipe(object currentValue, out object newValue)
+        {
+            newValue = currentValue;
+            IDictionaryService dictService = GetService<IDictionaryService>();
+            string recipeArgumentValue = dictService.GetValue(m_RecipeArgumentName) as string;
+            if (recipeArgumentValue == null)
+            {
+                return false;
+            }
+            newValue = SolZipHelper.GetZipFileName(recipeArgumentValue);
+            return newValue != currentValue;
+        }
+
+        #region IAttributesConfigurable Members
+
+        public void Configure(StringDictionary attributes)
+        {
+            m_RecipeArgumentName = attributes["RecipeArgument"];
+        }
+
+        #endregion
+    }
+}
