0ca65af3b12605838cc7bc58f1f852f0182f03c8
Jern_cp <jesper@niedermann.dk>
2009-06-08 17:35:23 +0000

    MME 90% finished

---
+++ SolZipMME/SolZipMenuManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.AddIn;
+using ManagedMenuAddInViews;
+using System.Windows.Forms;
+using System.IO;
+
+namespace SolZipMME
+{
+    [AddIn("SolZipManagedAddIn", Version = "1.0.0.0")]
+    public class SolZipMenuManager : MenuManagerAddInView 
+    {
+        public override List<MenuItemView> CreateMenus(MenuContextView context)
+        {
+            if (context.Levels == ContextLevels.Solution || context.Levels == ContextLevels.Project
+                || context.Levels == ContextLevels.Item)
+            {
+                return new List<MenuItemView>() { new SolZipMenuItem("Compress with Zip...") };
+            }
+            return null;
+        }
+
+        public override string MainMenu(ApplicationTypes types)
+        {
+            if (types != ApplicationTypes.VS2008)
+                return null;
+
+            return "SolutionZipper (MME)";
+        }
+
+        public override void MenuClicked(MenuItemView clickedMenu, MenuContextView menuContext)
+        {
+            var zipitForm = new SolZipMMEForm(menuContext.FullPath);
+            zipitForm.ShowDialog();
+        }
+    }
+}
