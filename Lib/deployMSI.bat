7e08f0f31bafdf022adc8317cc2041ea9c6c542b
Jern_cp <SND\Jern_cp>
2009-06-14 08:45:19 +0000

    Added bat file for deploying the msi files.

---
+++ Lib/deployMSI.bat
@@ -0,0 +1,6 @@
+MKDir C:\MSI
+xcopy /Y ..\SunZipSetup\Release\SunZipSetup.msi "C:\MSI\"
+xcopy /Y ..\SolZipMMESetup\Release\SolZipMMESetup.msi "C:\MSI\"
+xcopy /Y ..\SolZipGuidanceGATSetup\Release\SolZipGuidanceGATSetup.msi "C:\MSI\"
+
+
